396e7518180fe8b9eb2444e0e41747ceaec3c39d
liangweibin <390633336@qq.com>
2021-01-18 12:13:31 +0800

    add mvc to application

--- WebLeslieApp/Startup.cs
+++ WebLeslieApp/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebLeslieApp
 {
@@ -16,6 +17,8 @@ namespace WebLeslieApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,21 +50,18 @@ namespace WebLeslieApp
             //});
             app.UseRouting();
 
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello hahahah leslie!");
+            //    });
+            //});
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello hahahah leslie!");
+                    endpoints.MapDefaultControllerRoute();
                 });
-            });
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/ha", async context =>
-                {
-                    await context.Response.WriteAsync("HAHAH!");
-                });
-            });
         }
     }
 }
